ff88457a9d57f04cf3707539407da4ce736e0e03
Noah Cape <noahcape@gmail.com>
2024-07-19 16:26:06 +0300

    update examples with new API

--- examples/fork_fastq.rs
+++ examples/fork_fastq.rs
@@ -1,16 +1,21 @@
+use antisequence::expr::*;
+use antisequence::node::*;
 use antisequence::*;
 
 fn main() {
-    let (left, right) = iter_fastq1("example_data/simple.fastq", 1)
-        .unwrap_or_else(|e| panic!("{e}"))
-        .cut(sel!(), tr!(seq1.* -> seq1.a, seq1.b), LeftEnd(3))
-        .fork();
+    let mut right = Graph::new();
+    right.add(InputFastq1Node::new("example_data/simple.fastq").unwrap_or_else(|e| panic!("{e}")));
+    right.add(CutNode::new(tr!(seq1.* -> seq1.a, seq1.b), LeftEnd(3)));
+    right.add(TrimNode::new([label("seq1.a")]));
+    right.add(DbgNode::new());
 
-    let left = left
-        .set(sel!(), label!(name1.*), "{name1.*}_{seq1.a}")
-        .dbg(sel!());
+    let mut left = Graph::new();
+    left.add(SetNode::new(
+        label("name1.*"),
+        concat_all([Expr::from(label("name1.*")), Expr::from(label("seq1.a"))]),
+    ));
+    left.add(DbgNode::new());
+    right.add(ForkNode::new(left));
 
-    let right = right.trim(sel!(), [label!(seq1.a)]).dbg(sel!());
-
-    run!(left, right);
+    right.run().unwrap_or_else(|e| panic!("{e}"));
 }
