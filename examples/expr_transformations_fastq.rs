ae8d8272520d1e8a1711b22ca395e63c3e1d19a3
Noah Cape <noahcape@gmail.com>
2024-07-21 10:03:23 +0300

    add revcomp expr

--- examples/expr_transformations_fastq.rs
+++ examples/expr_transformations_fastq.rs
@@ -17,10 +17,17 @@ AAAAACCATTTTT
 
     g.add(CutNode::new(tr!(seq1.* -> seq1.a, seq1.b), LeftEnd(5)));
     g.add(CutNode::new(tr!(seq1.b -> seq1.mid, seq1.bb), LeftEnd(3)));
-    g.add(SetNode::new(label("seq1.mid"), Expr::from(label("seq1.mid")).rev()));
+    g.add(SetNode::new(
+        label("seq1.mid"),
+        Expr::from(label("seq1.mid")).rev(),
+    ));
     g.add(SetNode::new(
         label("seq1.mid"),
         Expr::from(label("seq1.mid")).slice(Expr::from(1)..),
+    ));
+    g.add(SetNode::new(
+        label("seq1.mid"),
+        Expr::from(label("seq1.mid")).revcomp(),
     ));
     g.add(DbgNode::new());
     g.add(OutputFastqNode::new1("example_output/simple.fastq"));
