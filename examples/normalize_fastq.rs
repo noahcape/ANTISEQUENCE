b814163090248ca20082baadc197ae3987dd31ab
Noah Cape <noahcape@gmail.com>
2024-07-21 18:25:45 +0300

    normalize example

---
+++ examples/normalize_fastq.rs
@@ -0,0 +1,32 @@
+use antisequence::expr::*;
+use antisequence::node::*;
+use antisequence::*;
+
+fn main() {
+    let fastq = b"@read1
+TTTTTT
++
+012345
+@read1a
+TTTTTT
++
+012345
+@read2
+AAAAAAA
++
+0123456
+@read3
+CCCCCCCC
++
+01234567";
+
+    let mut g = Graph::new();
+    g.add(InputFastq1Node::from_bytes(fastq).unwrap_or_else(|e| panic!("{e}")));
+    g.add(SetNode::new(
+        label("seq1.*"),
+        Expr::from(label("seq1.*")).normalize(Expr::from(6)..=Expr::from(8)),
+    ));
+    g.add(DbgNode::new());
+    g.add(OutputFastqNode::new1("example_output/simple.fastq"));
+    g.run().unwrap_or_else(|e| panic!("{e}"));
+}
