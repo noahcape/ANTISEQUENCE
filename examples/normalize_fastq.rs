351e3a60b318a4409f3e7aff6c47f22673124440
Noah Cape <noahcape@gmail.com>
2024-07-23 12:46:57 +0300

    normalize with use qual

--- examples/normalize_fastq.rs
+++ examples/normalize_fastq.rs
@@ -4,27 +4,32 @@ use antisequence::*;
 
 fn main() {
     let fastq = b"@read1
-TTTTTT
+TTTTTTGAC
 +
-012345
+012345678
 @read1a
-TTTTTT
+TTTTTTGAC
 +
-012345
+012345678
 @read2
-AAAAAAA
+AAAAAAAGAC
 +
-0123456
+0123456789
 @read3
-CCCCCCCC
+CCCCCCCCGAC
 +
-01234567";
+01234567890";
 
     let mut g = Graph::new();
     g.add(InputFastq1Node::from_bytes(fastq).unwrap_or_else(|e| panic!("{e}")));
+    g.add(MatchAnyNode::new(
+        tr!(seq1.* -> seq1.left, seq1.patt),
+        Patterns::from_strs(["GAC"]),
+        ExactSuffix,
+    ));
     g.add(SetNode::new(
-        label("seq1.*"),
-        Expr::from(label("seq1.*")).normalize(6..=8),
+        label("seq1.left"),
+        Expr::from(label("seq1.left")).normalize(6..=8),
     ));
     g.add(DbgNode::new());
     g.add(OutputFastqNode::new1("example_output/simple.fastq"));
